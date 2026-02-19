106fe3eb13c53fae4294f257715d60445d774e96
Cristina Silva Ungo <csu00002@red.ujaen.es>
2026-02-19 11:54:22 +0100

    Doble salto

--- monogame/Game1.cs
+++ monogame/Game1.cs
@@ -19,11 +19,19 @@ public class Game1 : Game
     Vector2 velocity;
 
     float velocidad = 4f;
-    float fuerza=-12f;
+    float fuerza=-8f;
     float gravedad=0.5f;
+    int saltos=0;
+    KeyboardState teclaanterior; //esto es para que si mantenemos presionado la w no haga doble salto
+    SpriteEffects efecto = SpriteEffects.None; //esto es para que se de la vuelta 
+    //Si usamos el flip horizontal se cambia muy bruscamente por lo que vamos a interpolar despacito
+    float escalaX = 1f;          // escala actual
+    float escalaObjetivo = 1f;   // hacia dónde queremos ir
+    float velocidadGiro = 0.2f;  // velocidad de interpolacion
+    float escala = 2f;
 
-    Boolean hay_suelo=true; //para poder saltar, esto controla el doble salto y no saltar infinitvamente
-    //solo saltamos si hay suelo debajo
+
+
 
     public Game1()
     {
@@ -36,7 +44,7 @@ public class Game1 : Game
     {
         // TODO: Add your initialization logic here
         //inicializamos la posición del personaje
-        position= new Vector2(100,100);
+        position = new Vector2(400, 300);
         base.Initialize();
     }
 
@@ -59,20 +67,24 @@ public class Game1 : Game
         if (keyboard.IsKeyDown(Keys.D)) //si le damos a la D vamos hacia delante
         {
             velocity.X = velocidad;
+            escalaObjetivo = 1f; // mirar derecha
         }
         else if (keyboard.IsKeyDown(Keys.A))
         {
             velocity.X = -velocidad;
+            escalaObjetivo = -1f; // mirar izquierda
         }
         else
         {
             velocity.X = 0;
         }
         // Salto
-        if (keyboard.IsKeyDown(Keys.W) && hay_suelo)//si hay suelo y le damos a la w saltamos
+        if (keyboard.IsKeyDown(Keys.W) && saltos<2 && teclaanterior.IsKeyUp(Keys.W))//si hay suelo y le damos a la w saltamos
         {
+            saltos++;
+           
             velocity.Y = fuerza; //como es un vector 2D, el movimiento en el eje Y la componente Y del vector
-            hay_suelo = false;
+            
         }
 
         // Aplicar gravedad
@@ -82,19 +94,38 @@ public class Game1 : Game
         position += velocity;
 
         // Suelo artificial (ejemplo: altura 300)
+        //cuando volvemos a tocar el suelo, se reinicia
+        float mitadAltura = (capybara.Height * 2f) / 2f;
         if (position.Y >= 300)
         {
             position.Y = 300;
             velocity.Y = 0;
-            hay_suelo = true;
+            saltos=0;
         }
 
 
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        // Si queremos cambiar de dirección
+        if (Math.Sign(escalaX) != Math.Sign(escalaObjetivo))
+        {
+            // Primero reducir hacia 0
+            escalaX = MathHelper.Lerp(escalaX, 0f, velocidadGiro);
 
+            // Cuando es casi 0, cambiamos de lado
+            if (Math.Abs(escalaX) < 0.05f)
+            {
+                escalaX = 0.05f * Math.Sign(escalaObjetivo);
+            }
+        }
+        else
+        {
+            // Luego expandimos hacia el objetivo
+            escalaX = MathHelper.Lerp(escalaX, escalaObjetivo, velocidadGiro);
+        }     
+           //lafuncion MathHelper.Lerp(a,b,c) va de a -> b el porcentaje indicado por c
+        teclaanterior=keyboard;
         base.Update(gameTime);
     }
 
@@ -106,7 +137,18 @@ public class Game1 : Game
         // TODO: Add your drawing code here
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
         //todo lo que hay aquí en mitad se dibuja, tiene que tener siempre un inicio y un fin
-        _spriteBatch.Draw(capybara, new Rectangle((int)position.X, (int)position.Y, 200,200),Color.White);
+
+        _spriteBatch.Draw(
+            capybara,
+            position,
+            null,
+            Color.White,
+            0f,
+            new Vector2(capybara.Width / 2f, capybara.Height / 2f),
+            new Vector2(escalaX * escala, escala),
+            SpriteEffects.None,
+            0f
+        );
         //queremos pintar capybara en la posición del vector y color blanco para que no se tinte
         //normalmente se usa Texture2D, vector, color. Pero usamos rectangle para poder redimensionar el objeto
         //si la ampliamos se va a ver borroso por lo que vamos a ver el mapping, para ello en begin usamos
