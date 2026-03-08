2cb8942d8617d7509752d648f699541585e20523
Cristina Silva Ungo <csu00002@red.ujaen.es>
2026-03-08 18:12:16 +0100

    Modularización del movimiento del capybara y comienzo del tilemap

--- monogame/Game1.cs
+++ monogame/Game1.cs
@@ -17,7 +17,6 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-<<<<<<< HEAD
     private Dictionary<Vector2, int> tilemap;
     private List<Rectangle> texturas;
     private Texture2D textureAtlas;
@@ -30,11 +29,6 @@ public class Game1 : Game
 
 
     MovedSprite personaje;
-
-=======
-
-    MovedSprite personaje;
->>>>>>> dd83b561479ee07caca0c5c640da583ced24d6f5
 
     float velocidad = 4f;
     float fuerza = -8f;
@@ -99,18 +93,9 @@ private Dictionary<Vector2, int> CargarMapa(string ruta)
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         Texture2D texturecapibara = Content.Load<Texture2D>("personaje_basico");
         personaje = new MovedSprite(texturecapibara, new Vector2(400, 300), escala, velocidad); //cargamos al capibara, pasando la textura, la posicion, la escala y la velocidad
-<<<<<<< HEAD
-
-
+        textureAtlas = Content.Load<Texture2D>("tilesheet");
         // TODO: use this.Content to load your game content here
-        capybara = Content.Load<Texture2D>("personaje_basico"); 
-        textureAtlas= Content.Load<Texture2D>("tilesheet");
-
-=======
-
->>>>>>> dd83b561479ee07caca0c5c640da583ced24d6f5
-    }
-
+        capybara = Content.Load<Texture2D>("personaje_basico"); }
     protected override void Update(GameTime gameTime)
     {
 
@@ -118,12 +103,13 @@ private Dictionary<Vector2, int> CargarMapa(string ruta)
 
 
         float sueloY = GraphicsDevice.Viewport.Height;
-        personaje.Update(tecladoActual, gravedad, sueloY, saltos, teclaanterior, fuerza);
+        personaje.Update(tecladoActual, gravedad, sueloY,ref saltos, teclaanterior, fuerza);
 
 
-        //salir del jeugo
-        if (tecladoActual.IsKeyDown(Keys.Escape))
-            Exit();
+        /// ← estas líneas deben estar aquí en Game1
+    if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || 
+        tecladoActual.IsKeyDown(Keys.Escape))
+        Exit();
 
         // actualizacion de la tecla anterior 
         teclaanterior = tecladoActual;
@@ -159,13 +145,23 @@ private Dictionary<Vector2, int> CargarMapa(string ruta)
 
         foreach (var item in tilemap)
         {
-            Rectangle dest= new(
-                (int)item.Key.X * 64-100,
-                (int)item.Key.Y * 64-100,
-                64,
-                64
+            int columnas = 8; // ancho del mapa en tiles 
+            int filas = 4;    // alto del mapa en tiles 
+            int tileSize = 64;
+
+            int mapaAncho = columnas * tileSize;
+            int mapaAlto = filas * tileSize;
+
+            int offsetX = (GraphicsDevice.Viewport.Width - mapaAncho) / 2;
+            int offsetY = (GraphicsDevice.Viewport.Height - mapaAlto) / 2;
+
+            Rectangle dest = new(
+                (int)item.Key.X * tileSize + offsetX,
+                (int)item.Key.Y * tileSize + offsetY,
+                tileSize,
+                tileSize
             );
-            Rectangle src = texturas[item.Value-1];
+            Rectangle src = texturas[item.Value - 1];
             _spriteBatch.Draw(textureAtlas, dest, src, Color.White);
         }  
         _spriteBatch.End();
--- monogame/movedsprite.cs
+++ monogame/movedsprite.cs
@@ -2,61 +2,79 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
 
-//clase para el movimiento del personaje
 namespace capybara
 {
     internal class MovedSprite : ScaledSprite
     {
-     public SpriteEffects efecto = SpriteEffects.None; //lo añadimos para el cambio de sentido del capibara
-    private float speed;
-    public Vector2 velocity; 
-    private KeyboardState prevKeyboard;
+        public SpriteEffects efecto = SpriteEffects.None;
+        private float speed;
+        public Vector2 velocity;
+        private KeyboardState prevKeyboard;
+        float escala = 2f;
+        
+        // Para suavizar el cambio de dirección
+        private float escalaX = 1f;        // escala visual horizontal (1 o -1)
+        private float escalaObjetivo = 1f; // hacia donde queremos ir
+        private float velocidadFlip = 8f;  // qué tan rápido cambia
 
-    public MovedSprite(Texture2D texture, Vector2 position, float scale, float speed) 
-        : base(texture, position, scale)
-    {
-        this.speed = speed;
-        this.velocity = Vector2.Zero;
-    }
-    public void Update(KeyboardState keyboard, float gravedad, float sueloY, int saltos, KeyboardState teclaanterior, float fuerza) //update con la logica del teclado
-    {
-        // mov izq y der
-        if (keyboard.IsKeyDown(Keys.D)){
-            velocity.X = speed;
-            efecto = SpriteEffects.None;
-        }
-        else if (keyboard.IsKeyDown(Keys.A)){
-            efecto = SpriteEffects.FlipHorizontally;
-            velocity.X = -speed;
-        }
-        else
-            velocity.X = 0;
-
-       // salto
-       if (keyboard.IsKeyDown(Keys.W) && saltos<2 && teclaanterior.IsKeyUp(Keys.W))//si hay suelo y le damos a la w saltamos
+        public MovedSprite(Texture2D texture, Vector2 position, float scale, float speed)
+            : base(texture, position, scale)
         {
-            saltos++;
-            velocity.Y = fuerza; //como es un vector 2D, el movimiento en el eje Y la componente Y del vector
-            
+            this.speed = speed;
+            this.velocity = Vector2.Zero;
+        }
+
+        public void Update(KeyboardState keyboard, float gravedad, float sueloY, ref int saltos, KeyboardState teclaanterior, float fuerza)
+        {
+            // Movimiento horizontal
+            if (keyboard.IsKeyDown(Keys.D))
+            {
+                velocity.X = speed;
+                escalaObjetivo = 1f;
+            }
+            else if (keyboard.IsKeyDown(Keys.A))
+            {
+                velocity.X = -speed;
+                escalaObjetivo = -1f;
+            }
+            else
+            {
+                velocity.X = 0;
+            }
+
+            // Interpolación suave del flip (en lugar de cambio brusco)
+            escalaX += (escalaObjetivo - escalaX) * velocidadFlip * 0.016f;
+            if (escalaX < 0)
+                efecto = SpriteEffects.FlipHorizontally;
+            else
+                efecto = SpriteEffects.None;
+
+            // Salto (máximo doble salto)
+            if (keyboard.IsKeyDown(Keys.W) && saltos < 2 && teclaanterior.IsKeyUp(Keys.W))
+            {
+                saltos++;
+                velocity.Y = fuerza;
+            }
+
+            // Gravedad
+            velocity.Y += gravedad;
+
+            // Aplicar movimiento
+            position += velocity;
+
+            // Suelo
+            float mitadAltura = (Rect.Height * escala) / 2f;
+            if (position.Y >= 450 - mitadAltura)
+        {
+            position.Y = 450 - mitadAltura;
+            velocity.Y = 0;
+            saltos = 0;
         }
         
-        velocity.Y += gravedad;
-        position += velocity;
 
-        // Suelo
-        float mitadAltura = Rect.Height / 2f;
-        if (position.Y >= sueloY - mitadAltura)
-        {
-            position.Y = sueloY - mitadAltura;
-            velocity.Y = 0;
+            prevKeyboard = keyboard;
         }
-
-        prevKeyboard = keyboard;
     }
-}
 }
